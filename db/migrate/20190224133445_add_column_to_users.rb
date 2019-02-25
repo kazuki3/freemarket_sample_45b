9b7b6b5b26bc77ef780f7c3fcf007985ce48c7f1
kenichi-taira <taira.kenichi@gmail.com>
2019-02-25 16:46:30 +0900

    [WIT] devise日本語化ファイル作成

--- db/migrate/20190224133445_add_column_to_users.rb
+++ db/migrate/20190224133445_add_column_to_users.rb
@@ -2,10 +2,10 @@ class AddColumnToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :nickname, :string, null: false,
     add_index :users, :nickname, unique: true
-    add_column :users, :family_name_kanji, :string, null: false,
-    add_column :users, :first_name_kanji, :string, null: false,
-    add_column :users, :family_name_kana, :string, null: false,
-    add_column :users, :first_name_kana, :string, null: false,
+    add_column :users, :first_kanji, :string, null: false,
+    add_column :users, :last_kanji, :string, null: false,
+    add_column :users, :first_kana, :string, null: false,
+    add_column :users, :last_kana, :string, null: false,
     add_column :users, :year, :date, null: false,
     add_column :users, :month, :date, null: false,
     add_column :users, :day, :date, null: false,

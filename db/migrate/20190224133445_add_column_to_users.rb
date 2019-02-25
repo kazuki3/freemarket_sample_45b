3a6af12fd6350ef0620fa6e57eae0e4adf13e19e
kenichi-taira <taira.kenichi@gmail.com>
2019-02-25 14:29:14 +0900

    [WIT] devise view 作成中

--- db/migrate/20190224133445_add_column_to_users.rb
+++ db/migrate/20190224133445_add_column_to_users.rb
@@ -1,5 +1,13 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :nickname, :string
+    add_column :users, :nickname, :string, null: false,
+    add_index :users, :nickname, unique: true
+    add_column :users, :family_name_kanji, :string, null: false,
+    add_column :users, :first_name_kanji, :string, null: false,
+    add_column :users, :family_name_kana, :string, null: false,
+    add_column :users, :first_name_kana, :string, null: false,
+    add_column :users, :year, :date, null: false,
+    add_column :users, :month, :date, null: false,
+    add_column :users, :day, :date, null: false,
   end
 end

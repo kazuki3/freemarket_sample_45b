6ae489edc45c8f0ede3c463c7416ad79403de205
kenichi-taira <taira.kenichi@gmail.com>
2019-02-26 10:48:50 +0900

    WIT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    end
+    # def configure_permitted_parameters
+    #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    # end
 
 end

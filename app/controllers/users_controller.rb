559f958f99ce53d0ce71f812076971183f96e929
kenichi-taira <taira.kenichi@gmail.com>
2019-02-27 19:42:26 +0900

    [WIT]deviseのコントローラー生成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: :new
 
   def index
 
@@ -33,5 +34,27 @@ class UsersController < ApplicationController
 
   end
 
+  private
+  def create_params
+    params.require(:profile).permit(first_name:string last_name:string first_name_kana:integer last_name_kana:integer birthdate:date zip_code:integer prefecture:string city:string ddress1:string ddress2:string phone_number:string user:references).merge(user_id: current_user.id)
+  end
 
+end
+
+
+  def new
+    @product = Product.find(params[:product_id])
+    @review = Review.new
+  end
+
+  def create
+    Review.create(create_params)
+
+    redirect_to controller: :products, action: :index
+  end
+
+  private
+  def create_params
+    params.require(:profile).permit(:rate, :review).merge(product_id: params[:product_id], user_id: current_user.id)
+  end
 end

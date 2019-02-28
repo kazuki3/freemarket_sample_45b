f683bc56c15e000d8eeed14503c7f98a954c7b5d
kenichi-taira <taira.kenichi@gmail.com>
2019-02-28 18:02:57 +0900

    [WIT] 連絡先情報入力ページ　DB書き込み確認済み

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,50 @@
+class ProfilesController < ApplicationController
+  before_action :profile_params, only: [:create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+
+  def index
+  end
+
+
+  def show
+  end
+
+
+  def new
+    @profile = Profile.new
+  end
+
+
+  def create
+    @profile = Profile.create(profile_params)
+      if @profile.save
+          sign_in(@profile, bypass: true)
+          redirect_to root_path
+      else
+        redirect_to new_profile_path
+    end
+  end
+
+
+  def edit
+    sign_in(current_user, bypass: true)
+    @profile = Profile.update(profile_params)
+  end
+
+
+  def update
+    sign_in(current_user, bypass: true)
+    @profile = Profile.update(profile_params)
+  end
+
+
+  def destroy
+  end
+
+
+  private
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :birthdate, :zip_code, :prefecture, :city, :address1, :address2, :phone_number, ).merge(user_id: current_user.id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,6 @@ Rails.application.routes.draw do
    :passwords => 'users/passwords'
   }
 
-  resources :users
-
-  # devise_scope :user do
-  #   get 'my_page' => 'users/registrations#my_page'
-  # end
+  resources :profiles
 
 end

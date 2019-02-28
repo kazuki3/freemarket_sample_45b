0c0c929c591ae21b8ada74e00f5e73a150f8cf7f
kenichi-taira <taira.kenichi@gmail.com>
2019-02-28 21:59:50 +0900

    [WIT] クレジットカード入力フォーム　途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
   # validatable :nickname, presence: true
   # validatable :email, presence: true
   # validatable :password, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,23 @@ Rails.application.routes.draw do
 
   root 'products#index'
 
-  get   '/show', to: 'products#show'
-
   devise_for :users, :controllers => {
-   :registrations => 'users/registrations',
-   :sessions => 'users/sessions',
-   :passwords => 'users/passwords'
+   :registrations => 'users/registrations',:sessions => 'users/sessions',:passwords => 'users/passwords'
   }
 
+  devise_scope :user do
+    post '/users' => 'users/registrations#create'
+  end
+
   resources :profiles
+  resources :payments
+  get   '/show', to: 'products#show'
+
+  # pay.jp用の記述（作成の時のために置いておく）メモ：平良
+  # resources :payments do only: [:index, :new, :update, :create] do
+  #   collection do
+  #     post 'pay'
+  #   end
+  # end
 
 end

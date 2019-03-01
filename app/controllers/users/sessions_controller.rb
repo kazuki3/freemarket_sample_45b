e33fe06906e436405d0487450b738421d1562016
kenichi-taira <taira.kenichi@gmail.com>
2019-03-01 17:55:23 +0900

    Merge branch 'kazuki3/master'
    # Conflicts:
    #	.ruby-version

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,14 @@
 
 class Users::SessionsController < Devise::SessionsController
-
+  before_action :create_params, only: [:create]
+  before_action :authenticate_user!, only: [:edit, :update]
 
   # GET /resource/sign_in
   def new
-    super
   end
+
+  def create
+  end
+
 
 end

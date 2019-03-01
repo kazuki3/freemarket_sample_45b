cb0135e4a35978b1fce8873bc90dd4ce8367ae7c
kenichi-taira <taira.kenichi@gmail.com>
2019-03-01 16:19:24 +0900

    [WIT] View調整中

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -26,6 +26,7 @@ class PaymentsController < ApplicationController
     params.require(:payment).permit(:card_number, :expiration_date, :security_code, ).merge(user_id: current_user.id)
   end
 
+# 下記は今後の使用予定のため、コメントアウトにて置きます。
   # def pay
   #     Payjp.api_key = '秘密キー'
   #     charge = Payjp::Charge.create(

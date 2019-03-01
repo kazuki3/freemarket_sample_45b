c924a11d527bbfaf4bf79eae84e214a6a9f5fea6
kenichi-taira <taira.kenichi@gmail.com>
2019-03-01 16:13:51 +0900

    コメントアウト、不要なコード削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-
-  # validatable :nickname, presence: true
-  # validatable :email, presence: true
-  # validatable :password, presence: true
-  # validatable :password_confirmation, presence: true
 
 end

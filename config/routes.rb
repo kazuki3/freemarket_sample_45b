33431876be27de717d0e8a258b2b0d55a140fc84
kenichi-taira <taira.kenichi@gmail.com>
2019-03-03 19:22:02 +0900

    [WIT] 調整（リダイレクト先）

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   root 'products#index'
-
   resources :products
+  resources :profiles
 
   devise_for :users, :controllers => {
    :registrations => 'users/registrations',:sessions => 'users/sessions',:passwords => 'users/passwords'
@@ -10,12 +10,20 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get   'users/index', to: 'users/registrations#index'
+    get   'users/signout', to: 'users/registrations#signout'
     post '/users' => 'users/registrations#create'
   end
 
-  resources :profiles
   resources :payments
   get   '/show', to: 'products#show'
+
+  # resources :profiles do
+  #   collection do
+  #     get 'signout'
+  #   end
+  # end
+
+end
 
   # pay.jp用の記述（作成の時のために置いておく）メモ：平良
   # resources :payments do only: [:index, :new, :update, :create] do
@@ -23,5 +31,3 @@ Rails.application.routes.draw do
   #     post 'pay'
   #   end
   # end
-
-end

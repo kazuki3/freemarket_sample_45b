bbfd77423a4ff7ad715a1c876745b97d98853518
kenichi-taira <taira.kenichi@gmail.com>
2019-03-04 13:47:10 +0900

    [WIT]deviseやり直し（ルート設定し直し）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protect_from_forgery with: :exception
 
 private
@@ -10,23 +12,27 @@ private
     end
   end
 
-  before_action :basic_auth, if: :production?
-  protect_from_forgery with: :exception
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :self_introduction])
+  end
 
-  private
   def production?
     Rails.env.production?
   end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password = ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
+  # protect_from_forgery with: :exception
 
-  def after_sign_out_path_for(resource)
-    new_user_session_path
-  end
+  # private
+
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV["BASIC_AUTH_USER"] && password = ENV["BASIC_AUTH_PASSWORD"]
+  #   end
+  # end
+
+  # def after_sign_out_path_for(resource)
+  #   new_user_session_path
+  # end
 
 
 end

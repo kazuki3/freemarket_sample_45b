c9d22258774d7b7d02dec4d23802e2521f7da3c8
kenichi-taira <taira.kenichi@gmail.com>
2019-03-06 13:39:37 +0900

    Facebook,google認証設定記述（ブランチ間違いのためデータ引き渡し用）

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
+    devise :omniauthable, omniauth_providers: %i[facebook twitter google_oauth2]
 
   # You should also create an action method in this controller like this:
   # def twitter
@@ -21,10 +22,20 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   #   super
   # end
 
+  # omniauthのコールバック時に呼ばれるメソッド
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
   # protected
 
   # The path used when OmniAuth fails
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
+
+
 end

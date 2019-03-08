9d8207c51597dd9bce231ffb6465bfc8fdc54bbb
Ryusk22 <kuppo0211@docomo.ne.jp>
2019-03-08 16:44:09 +0900

    Execute migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20190302104211) do
 
   create_table "payments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|

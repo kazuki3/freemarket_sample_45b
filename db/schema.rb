cf9658c1797b4f65dace448c0cdc7ccea64a44c8
asahi0229 <yuuto.asa.6794@gmail.com>
2019-03-08 17:21:28 +0900

    コンフリクト

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 0) do
-
-=======
 ActiveRecord::Schema.define(version: 20190302104211) do
 
   create_table "payments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -61,5 +57,4 @@ ActiveRecord::Schema.define(version: 20190302104211) do
   end
 
   add_foreign_key "profiles", "users"
->>>>>>> kazuki3/master
 end

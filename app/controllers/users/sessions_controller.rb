4e73e4a731fa1fbde39ff5176ee9aeb297fb00a4
asahi0229 <yuuto.asa.6794@gmail.com>
2019-03-10 19:42:54 +0900

    追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -27,14 +27,6 @@ class Users::SessionsController < Devise::SessionsController
         redirect_to new_user_session_path
     end
   end
-
-<<<<<<< HEAD
-=======
-  # before_action :configure_sign_in_params, only: [:create]
-
- 
->>>>>>> origin/asahi
-
   # GET /resource/sign_in
   # def new
   #   super

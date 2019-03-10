b925affcadd41d7e1bac828f3b8426f0f61d097c
kenichi-taira <taira.kenichi@gmail.com>
2019-03-10 14:21:37 +0900

    Merge branch 'kazuki3/master' into taira

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,7 @@
 class Product < ApplicationRecord
-  enum condition: {}
-  enum shipping_method: {}
-  enum postage: {}
-  enum region: {}
-  enum date: {}
+
+  belongs_to :user
+  belongs_to :category
+  has_many :images
 
 end

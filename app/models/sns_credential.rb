65b4333fb2b9d32398c76447161983cdf588f8a1
Ychan0114 <nag.kaz01@ezweb.ne.jp>
2019-03-12 06:21:52 +0900

    Add self.find_sns and self.create_sns

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -1,3 +1,20 @@
 class SnsCredential < ApplicationRecord
   belongs_to :user
+
+  def self.find_sns(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    return snscredential
+  end
+
+  def self.create_sns(auth)
+    user = User.where(email: auth.info.email).first
+    SnsCredential.create(
+      uid: auth.uid,
+      provider: auth.provider,
+      user_id: user.id
+      )
+    return user
+  end
 end

5a80b12a6a3406d19d17caf7a97e7700dc92205c
Ychan0114 <nag.kaz01@ezweb.ne.jp>
2019-03-12 05:05:21 +0900

    Edit create_oauth processing

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,33 +15,17 @@ class User < ApplicationRecord
   has_many :sns_credentials
 
   def self.create_oauth(auth)
-    uid = auth.uid
-    provider = auth.provider
-    snscredential = SnsCredential.where(uid: uid, provider: provider).first
-    if snscredential.present?
-      user = User.where(id: snscredential.user_id).first
-    else
-      user = User.where(email: auth.info.email).first
-      if user.present?
-        SnsCredential.create(
-          uid: uid,
-          provider: provider,
-          user_id: user.id
-          )
-      else
-        user = User.create(
-          nickname: auth.info.name,
-          email:    auth.info.email,
-          password: Devise.friendly_token[0, 20],
-          )
-        SnsCredential.create(
-          uid: uid,
-          provider: provider,
-          user_id: user.id
-          )
-      end
-    end
-    return user
+  user = User.create(
+    nickname: auth.info.name,
+    email:    auth.info.email,
+    password: Devise.friendly_token[0, 20]
+    )
+  SnsCredential.create(
+    uid: auth.uid,
+    provider: auth.provider,
+    user_id: user.id
+    )
+  return user
   end
 
   def self.find_oauth(auth)

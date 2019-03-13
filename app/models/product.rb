b6a8fd415bf9e441516c5c00b52d437861c4a393
KAZUKI OKAYAMA <okymkzk0816@gmail.com>
2019-03-13 15:47:48 +0900

    Enum日本語化

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  
+
   belongs_to :user, foreign_key: :seller_id
   belongs_to :category
   belongs_to :prefecture
@@ -7,7 +7,7 @@ class Product < ApplicationRecord
   belongs_to :shipping_method
   has_many :images, inverse_of: :product, dependent: :destroy
   accepts_nested_attributes_for :images
-  
+
   validates :name, presence: true
   validates :detail, presence: true
   validates :detail,    length: { maximum: 1000 }
@@ -25,11 +25,11 @@ class Product < ApplicationRecord
     no_dirt: 3,
     bit_dirt: 4,
     dirt: 5,
-    bad: 6,
+    bad: 6
   }
 
   enum date: {
-    oneday_two_day: 1,
+    oneday_twoday: 1,
     twoday_threeday: 2,
     fourday_sevenday: 3
   }

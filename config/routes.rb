332e86eef8345f19e48fece37d37c07497809dbf
kenichi-taira <taira.kenichi@gmail.com>
2019-03-14 17:58:34 +0900

    MasterDateFork後のルート設定変更、TOPページボタン内へのURL付与、reCAPTCHA実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
 
 devise_for :users, :controllers => {
+ :omniauth_callbacks => 'users/omniauth_callbacks',
  :registrations => 'users/registrations',
- :sessions => 'users/sessions',
- :omniauth_callbacks => "users/omniauth_callbacks"
+ :sessions => 'users/sessions'
 }
 
   root 'products#index'
     resources :products do
     collection do
-      get 'buy'
-      get 'category'
-      get 'postage'
+    get 'buy'
     end
   end
 
@@ -19,13 +17,6 @@ devise_for :users, :controllers => {
   resources :profiles
   resources :payments
   get   'user/signout', to: 'users#signout'
-  get   'index', to: 'users#index'
-  get   'index2', to: 'users#registration_select'
-
-
-  # devise_scope :user do
-  #   post '/users', to: 'users/registrations#create'
-  # end
-
+  get   'select', to: 'users#registration_select', as: 'registration_select'
 
 end

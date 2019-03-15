7c7c4129efa7dd52ef1522cdea247907905ea57d
kenichi-taira <taira.kenichi@gmail.com>
2019-03-15 18:30:57 +0900

    Specファイル補修分

---
+++ app/models/sns_credential.rb
@@ -0,0 +1,34 @@
+class SnsCredential < ApplicationRecord
+  belongs_to :user
+
+
+  validates :uid, presence: true, uniqueness: true
+  validates :provider, presence: true
+
+
+  def self.find_sns(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    return snscredential
+  end
+
+  def self.create_sns(auth)
+    user = User.where(email: auth.info.email).first
+    SnsCredential.create(
+      uid: auth.uid,
+      provider: auth.provider,
+      user_id: user.id
+      )
+    return user
+  end
+
+  def self.check_sns(snscredential, oauth)
+    if snscredential.present?
+      @user = User.where(id: snscredential.user_id).first
+    else
+      @user = SnsCredential.create_sns(oauth)
+    end
+    return @user
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :user, class: User do
+    email                 {"test_user@gmail.com"}
+    nickname              {"mercari"}
+    password              {"000000"}
+    password_confirmation {"000000"}
+  end
+
+end

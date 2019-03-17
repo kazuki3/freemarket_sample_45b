c777b1cb116d603a1290ffde9da1df8fa84e9143
Ychan0114 <nag.kaz01@ezweb.ne.jp>
2019-03-17 16:35:25 +0900

    Add storage type

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
+  storage :file if Rails.env.development? || Rails.env.test?
+  storage :fog if Rails.env.production?
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"

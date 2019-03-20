a530aa3fea497e8efb94c6df111cce32ea2c28f8
Ychan0114 <nag.kaz01@ezweb.ne.jp>
2019-03-20 20:59:23 +0900

    Add server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 server '54.64.150.97', user: 'ec2-user', roles: %w{app db web}
+server '52.196.209.2', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"

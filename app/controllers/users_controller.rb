654789ad79ec24682abda7ada5cccc2b687cbbf1
KAZUKI OKAYAMA <okymkzk0816@gmail.com>
2019-03-21 00:13:05 +0900

    ユーザー編集機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,26 @@ class UsersController < ApplicationController
   def signout
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(update_user_params) if user.id == current_user.id
+    redirect_to edit_user_registration_path
+  end
+
 
   def registration_select
     if user_signed_in?
     redirect_to root_path, notice: 'すでにログインしています。'
     end
+  end
+
+  private
+  def update_user_params
+    params.require(:user).permit(:nickname, :self_introduction)
   end
 
 end
